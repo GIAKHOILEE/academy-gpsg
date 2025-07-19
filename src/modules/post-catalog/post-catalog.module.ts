9ba7b37dfca6db2313f662451b11a7f9bafcc015
GIAKHOILEE <giakhoilee@gmail.com>
2025-07-19 13:01:47 +0700

    fix

--- src/modules/post-catalog/post-catalog.module.ts
+++ src/modules/post-catalog/post-catalog.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { PostCatalog } from './post-catalog.entity'
 import { PostCatalogAdminController, PostCatalogController } from './post-catalog.controller'
 import { PostCatalogService } from './post-catalog.service'
+import { Post } from '@modules/post/post.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostCatalog])],
+  imports: [TypeOrmModule.forFeature([PostCatalog, Post])],
   controllers: [PostCatalogAdminController, PostCatalogController],
   providers: [PostCatalogService],
 })
