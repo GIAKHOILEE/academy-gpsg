4454697b0c6d804b03bd5329ad9bf79201df7bf6
GIAKHOILEE <giakhoilee@gmail.com>
2025-07-22 10:36:10 +0700

    fix

--- src/guards/optional-jwt-auth.guard.ts
+++ src/guards/optional-jwt-auth.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(user) {
+  handleRequest(err, user, info) {
     // Không throw error nếu không có token → trả undefined
     return user || null
   }
