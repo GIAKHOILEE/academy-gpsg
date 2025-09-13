b4e5f4071f25f48f9dfd28c95c4d4382794398b5
GIAKHOILEE <giakhoilee@gmail.com>
2025-09-13 12:09:22 +0700

    update exam

--- src/modules/class-rules/exam/_exam/exam.controller.ts
+++ src/modules/class-rules/exam/_exam/exam.controller.ts
@@ -68,6 +68,66 @@ export class AdminExamController {
   }
 }
 
+@Controller('teacher/exam')
+@ApiTags('Teacher Exam')
+@ApiBearerAuth()
+@Auth(Role.TEACHER)
+export class TeacherExamController {
+  constructor(private readonly examService: ExamService) {}
+  @Post()
+  @ApiOperation({ summary: 'Tạo bài kiểm tra' })
+  async createExam(@Body() createExamDto: CreateExamDto): Promise<ResponseDto> {
+    const exam = await this.examService.createExam(createExamDto)
+    return new ResponseDto({
+      statusCode: HttpStatus.CREATED,
+      messageCode: 'EXAM_CREATED_SUCCESSFULLY',
+      data: exam,
+    })
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Lấy tất cả bài kiểm tra' })
+  async getExams(@Query() paginateExamDto: PaginateExamDto): Promise<ResponseDto> {
+    const exams = await this.examService.getExams(paginateExamDto)
+    return new ResponseDto({
+      statusCode: HttpStatus.OK,
+      messageCode: 'EXAMS_FOUND_SUCCESSFULLY',
+      data: exams.data,
+      meta: exams.meta,
+    })
+  }
+  @Get(':id')
+  @ApiOperation({ summary: 'Lấy bài kiểm tra theo id' })
+  async getExamById(@Param('id') id: number): Promise<ResponseDto> {
+    const exam = await this.examService.getExamById(id)
+    return new ResponseDto({
+      statusCode: HttpStatus.OK,
+      messageCode: 'EXAM_FOUND_SUCCESSFULLY',
+      data: exam,
+    })
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Cập nhật bài kiểm tra' })
+  async updateExam(@Param('id') id: number, @Body() updateExamDto: UpdateExamDto): Promise<ResponseDto> {
+    await this.examService.updateExam(id, updateExamDto)
+    return new ResponseDto({
+      statusCode: HttpStatus.OK,
+      messageCode: 'EXAM_UPDATED_SUCCESSFULLY',
+    })
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Xóa bài kiểm tra' })
+  async deleteExam(@Param('id') id: number): Promise<ResponseDto> {
+    await this.examService.deleteExam(id)
+    return new ResponseDto({
+      statusCode: HttpStatus.OK,
+      messageCode: 'EXAM_DELETED_SUCCESSFULLY',
+    })
+  }
+}
+
 @Controller('exam')
 @ApiTags('User Exam')
 @ApiBearerAuth()
--- src/modules/class-rules/exam/_exam/exam.module.ts
+++ src/modules/class-rules/exam/_exam/exam.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Exam } from './exam.entity'
 import { ExamService } from './exam.service'
-import { AdminExamController, UserExamController } from './exam.controller'
+import { AdminExamController, TeacherExamController, UserExamController } from './exam.controller'
 import { ExamScore } from '../exam-scores/exam-scores.entity'
 import { Classes } from '@modules/class/class.entity'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Exam, ExamScore, Classes])],
   providers: [ExamService],
-  controllers: [AdminExamController, UserExamController],
+  controllers: [AdminExamController, TeacherExamController, UserExamController],
   exports: [ExamService],
 })
 export class ExamModule {}
