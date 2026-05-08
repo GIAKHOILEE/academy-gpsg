74f54eece31ae9024d4720c61beb339302571b01
GK <giakhoilee@gmail.com>
2026-05-08 10:06:39 +0700

    update: add evaluation/questions/statistics at home

--- src/modules/evaluation/questions/questions.controller.ts
+++ src/modules/evaluation/questions/questions.controller.ts
@@ -140,3 +140,20 @@ export class AdminQuestionsStatisticsController {
     })
   }
 }
+
+@ApiTags('User Questions Statistics')
+@Controller('evaluation/questions/statistics')
+export class UserQuestionsStatisticsController {
+  constructor(private readonly questionsStatisticsService: QuestionsStatisticsService) {}
+
+  @Get()
+  async statisticQuestion(@Query() paginateQuestionsStatisticsDto: PaginateQuestionsStatisticsDto): Promise<ResponseDto> {
+    const result = await this.questionsStatisticsService.statisticQuestion(paginateQuestionsStatisticsDto)
+    return new ResponseDto({
+      statusCode: HttpStatus.OK,
+      messageCode: 'QUESTIONS_STATISTICS_FETCHED_SUCCESSFULLY',
+      data: result.data,
+      meta: result.meta,
+    })
+  }
+}
